8641778ef6306465e252e47815d417596625e3b3
Marco André Mendes <marcoandre@gmail.com>
2026-09-15 11:16:35 -0300

    feat(api): adiciona busca e ordenação

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ProdutoFilter
@@ -9,5 +10,8 @@ from .serializers import ProdutoSerializer
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
     filterset_class = ProdutoFilter
+    ordering_fields = ("nome", "preco")
+    ordering = ("id",)
+    search_fields = ("nome",)
