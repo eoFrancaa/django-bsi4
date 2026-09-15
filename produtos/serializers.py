75af42659c7f83c7b61f5e34a1c36b8cd72a7774
Marco André Mendes <marcoandre@gmail.com>
2026-09-15 10:48:47 -0300

    feat(api): adiciona validações de produtos

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from decimal import Decimal
+
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from .models import Produto
 
@@ -7,3 +9,16 @@ class ProdutoSerializer(ModelSerializer):
     class Meta:
         model = Produto
         fields = ("id", "nome", "preco")
+
+    def validate_nome(self, value):
+        nome_limpo = value.strip()
+        if len(nome_limpo) < 2:
+            raise ValidationError(
+                "O nome deve possuir pelo menos 2 caracteres."
+            )
+        return nome_limpo
+
+    def validate_preco(self, value):
+        if value <= Decimal("0"):
+            raise ValidationError("O preço deve ser maior que zero.")
+        return value
